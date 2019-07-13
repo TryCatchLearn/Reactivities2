7f9509a2a9bc3a5e8ffe2b130c919d4b91cc4942
trycatchlearn <trycatchlearn@outlook.com>
2019-07-13 16:00:55 +0700

    End of Section 16

--- Application/User/CurrentUser.cs
+++ Application/User/CurrentUser.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Interfaces;
@@ -34,7 +35,7 @@ namespace Application.User
                     DisplayName = user.DisplayName,
                     Username = user.UserName,
                     Token = _jwtGenerator.CreateToken(user),
-                    Image = null
+                    Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
                 };
             }
         }
