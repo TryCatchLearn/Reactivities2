fa1a3b0a0893a547d2ab9dc8014b1d9c9ec1a51a
trycatchlearn <trycatchlearn@outlook.com>
2019-07-18 13:16:08 +0700

    End of section 21

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
-import { IActivity } from '../models/activity';
+import { IActivity, IActivitiesEnvelope } from '../models/activity';
 import { history } from '../..';
 import { toast } from 'react-toastify';
 import { IUser, IUserFormValues } from '../models/user';
@@ -7,52 +7,80 @@ import { IProfile, IPhoto } from '../models/profile';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
-axios.interceptors.request.use((config) => {
-  const token = window.localStorage.getItem('jwt');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config
-}, error => {
-  return Promise.reject(error);
-})
-
-axios.interceptors.response.use(undefined, (error) => {
-  if (error.message === 'Network Error' && !error.response) {
-    toast.error('Network error - make sure API is running!')
+axios.interceptors.request.use(
+  config => {
+    const token = window.localStorage.getItem('jwt');
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
   }
-  const {status, data, config} = error.response;
+);
+
+axios.interceptors.response.use(undefined, error => {
+  if (error.message === 'Network Error' && !error.response) {
+    toast.error('Network error - make sure API is running!');
+  }
+  const { status, data, config } = error.response;
   if (status === 404) {
     history.push('/notfound');
   }
-  if (status === 400 && config.method === 'get' && data.errors.hasOwnProperty('id')) {
+  if (
+    status === 400 &&
+    config.method === 'get' &&
+    data.errors.hasOwnProperty('id')
+  ) {
     history.push('/notfound');
   }
   if (status === 500) {
-    toast.error('Server error - check the terminal for more info!')
+    toast.error('Server error - check the terminal for more info!');
   }
   throw error.response;
-})
+});
 
 const responseBody = (response: AxiosResponse) => response.data;
 
-const sleep = (ms: number) => (response: AxiosResponse) => 
-    new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms));
+const sleep = (ms: number) => (response: AxiosResponse) =>
+  new Promise<AxiosResponse>(resolve =>
+    setTimeout(() => resolve(response), ms)
+  );
 
 const requests = {
-  get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
-  post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
-  put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
-  del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody),
+  get: (url: string) =>
+    axios
+      .get(url)
+      .then(sleep(1000))
+      .then(responseBody),
+  post: (url: string, body: {}) =>
+    axios
+      .post(url, body)
+      .then(sleep(1000))
+      .then(responseBody),
+  put: (url: string, body: {}) =>
+    axios
+      .put(url, body)
+      .then(sleep(1000))
+      .then(responseBody),
+  del: (url: string) =>
+    axios
+      .delete(url)
+      .then(sleep(1000))
+      .then(responseBody),
   postForm: (url: string, file: Blob) => {
     let formData = new FormData();
     formData.append('File', file);
-    return axios.post(url, formData, {
-      headers: {'Content-type': 'multipart/form-data'}
-    }).then(responseBody)
+    return axios
+      .post(url, formData, {
+        headers: { 'Content-type': 'multipart/form-data' }
+      })
+      .then(responseBody);
   }
 };
 
 const Activities = {
-  list: (): Promise<IActivity[]> => requests.get('/activities'),
+  list: (params: URLSearchParams): Promise<IActivitiesEnvelope> =>
+    axios.get('/activities', {params: params}).then(sleep(1000)).then(responseBody),
   details: (id: string) => requests.get(`/activities/${id}`),
   create: (activity: IActivity) => requests.post('/activities', activity),
   update: (activity: IActivity) =>
@@ -64,20 +92,29 @@ const Activities = {
 
 const User = {
   current: (): Promise<IUser> => requests.get('/user'),
-  login: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/login`, user),
-  register: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/register`, user),
-}
+  login: (user: IUserFormValues): Promise<IUser> =>
+    requests.post(`/user/login`, user),
+  register: (user: IUserFormValues): Promise<IUser> =>
+    requests.post(`/user/register`, user)
+};
 
 const Profiles = {
-  get: (username: string): Promise<IProfile> => requests.get(`/profiles/${username}`),
-  uploadPhoto: (photo: Blob): Promise<IPhoto> => requests.postForm(`/photos`, photo),
+  get: (username: string): Promise<IProfile> =>
+    requests.get(`/profiles/${username}`),
+  uploadPhoto: (photo: Blob): Promise<IPhoto> =>
+    requests.postForm(`/photos`, photo),
   setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
   deletePhoto: (id: string) => requests.del(`/photos/${id}`),
-  updateProfile: (profile: Partial<IProfile>) => requests.put(`/profiles`, profile),
-  follow: (username: string) => requests.post(`/profiles/${username}/follow`, {}),
+  updateProfile: (profile: Partial<IProfile>) =>
+    requests.put(`/profiles`, profile),
+  follow: (username: string) =>
+    requests.post(`/profiles/${username}/follow`, {}),
   unfollow: (username: string) => requests.del(`/profiles/${username}/follow`),
-  listFollowings: (username: string, predicate: string) => requests.get(`/profiles/${username}/follow?predicate=${predicate}`)
-}
+  listFollowings: (username: string, predicate: string) =>
+    requests.get(`/profiles/${username}/follow?predicate=${predicate}`),
+  listActivities: (username: string, predicate: string) =>
+    requests.get(`/profiles/${username}/activities?predicate=${predicate}`)
+};
 
 export default {
   Activities,
--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -3,23 +3,24 @@ import { Tab } from 'semantic-ui-react';
 import ProfilePhotos from './ProfilePhotos';
 import ProfileDescription from './ProfileDescription';
 import ProfileFollowings from './ProfileFollowings';
+import ProfileActivities from './ProfileActivities';
 
 const panes = [
   { menuItem: 'About', render: () => <ProfileDescription /> },
   { menuItem: 'Photos', render: () => <ProfilePhotos /> },
   {
     menuItem: 'Activities',
-    render: () => <Tab.Pane>Activities content</Tab.Pane>
+    render: () => <ProfileActivities />
   },
   { menuItem: 'Followers', render: () => <ProfileFollowings /> },
   { menuItem: 'Following', render: () => <ProfileFollowings /> }
 ];
 
 interface IProps {
-    setActiveTab: (activeIndex: any) => void;
+  setActiveTab: (activeIndex: any) => void;
 }
 
-const ProfileContent: React.FC<IProps> = ({setActiveTab}) => {
+const ProfileContent: React.FC<IProps> = ({ setActiveTab }) => {
   return (
     <Tab
       menu={{ fluid: true, vertical: true }}
